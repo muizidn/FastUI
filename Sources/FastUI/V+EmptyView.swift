484462e47f3483742ba095171af45c91f54369ca
muizidn <muiz.idn@gmail.com>
2020-01-28 17:09:53 +0700

    EmptyView

--- Sources/FastUI/V+EmptyView.swift
+++ Sources/FastUI/V+EmptyView.swift
@@ -1,9 +1,10 @@
-import UIKit
+import Foundation
+import FastUIKit
 
 public struct EmptyView: SomeView, UIKitContainer {
     public var view: UIView { return _view }
-    public let _view: UIView
-    public static func create(_ view: UIView, subViews: [SomeView]) -> EmptyView {
+    public let _view: FastUIKit.EmptyView
+    public static func create(_ view: FastUIKit.EmptyView, subViews: [SomeView]) -> EmptyView {
         if !subViews.isEmpty { fatalError() }
         return Self.init(_view: view)
     }
